8775c4965c4548e7c1690c61ce39a30fce36d035
José Sardagna <sardagna@MacBook-Pro-de-Jose.local>
2024-10-10 17:46:48 -0300

    paragem

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -86,7 +86,6 @@ func main() {
 	// Adicionar qualquer outra limpeza necessária (fechar conexões, encerrar goroutines, etc.)
 	time.Sleep(1 * time.Second) // Simular uma tarefa de limpeza
 	log.Println("Aplicação finalizada com sucesso.")
-	select {}
 }
 
 // Função para capturar e registrar o erro no arquivo de log
